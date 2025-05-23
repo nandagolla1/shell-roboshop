fb80b0c277591763e191777078bebe39b5780c90
nanda <nandagolla074@gmail.com>
2025-05-23 22:30:42 +0530

    data types

--- frontend.sh
+++ frontend.sh
@@ -59,34 +59,34 @@ VALIDATE(){
 }
 
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>> $LOG_FILE
 VALIDATE $? "Disabling default nginx package"
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>> $LOG_FILE
 VALIDATE $? "enabling nginx:1.24 version "
 
 PACKAGE_INSTALLER nginx
 
-systemctl enable nginx 
+systemctl enable nginx &>> $LOG_FILE
 VALIDATE $? "nginx system service enabling"
 
-systemctl start nginx 
+systemctl start nginx &>> $LOG_FILE
 VALIDATE $? "nginx system service starting"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
 
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOG_FILE
 VALIDATE $? "Downloading application code"
 
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html &>> $LOG_FILE
 VALIDATE $? "change current directory to the /usr/share/nginx/html directory"
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> $LOG_FILE
 VALIDATE $? "unzipping the application code to /usr/share/nginx/html directory"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>> $LOG_FILE
 VALIDATE $? "updating nginx conf file"
 
-systemctl restart nginx 
+systemctl restart nginx &>> $LOG_FILE
 VALIDATE $? "Restarting nginx"
