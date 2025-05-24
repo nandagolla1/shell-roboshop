469fe3e30aecf51b655a88d85b5431a740f52c41
nanda <nandagolla074@gmail.com>
2025-05-24 11:31:20 +0530

    data types

--- catalogue.sh
+++ catalogue.sh
@@ -115,15 +115,15 @@ PACKAGE_INSTALLER mongodb-mongosh &>> $LOG_FILE
 VALIDATE $? "downloading and installing mongo client"
 
 
-mongosh --host mongodb.nanda.cyou </app/db/master-data.js
+
 
 #=========the below condition is to check whether a database named catalogue exists on a MongoDB server.================
 
-#STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-#if [ $STATUS -lt 0 ]
-#then
-#    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
-#    VALIDATE $? "Loading data into MongoDB"
-#else
-#    echo -e "Data is already loaded ... $Y SKIPPING $N"
-#fi
+STATUS=$(mongosh --host mongodb.nanda.cyou --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.nanda.cyou </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
